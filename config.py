4962cb0b8636a3a047d69a19a420a50cd9575ae6
Chethan N <chethannagraj78@gmail.com>
2026-01-31 00:41:58 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL")
