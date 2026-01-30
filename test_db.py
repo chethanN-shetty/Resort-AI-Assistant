6168498d30aecade826477bc4cf7a42e879b9b29
Chethan N <chethannagraj78@gmail.com>
2026-01-31 00:41:45 +0530

    Delete test_db.py

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from app.database import engine
-
-try:
-    connection = engine.connect()
-    print("Database connected successfully!")
-    connection.close()
-except Exception as e:
-    print("Database connection failed:", e)
